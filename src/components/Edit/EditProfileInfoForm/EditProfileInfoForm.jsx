9929f51e876bdd71ca5d973e3049e67e1ee80e11
Douglas Vidal <vidaldl01@gmail.com>
2023-07-04 18:10:22 -0300

    notifications

--- src/components/Edit/EditProfileInfoForm/EditProfileInfoForm.jsx
+++ src/components/Edit/EditProfileInfoForm/EditProfileInfoForm.jsx
@@ -2,79 +2,112 @@ import React, { useState } from "react";
 import { Container, Form, Card, Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { addProfile } from "../../../redux/profileSlice";
+import { Store as notification } from 'react-notifications-component';
 
 const EditProfileInfoForm = () => {
-  const [imgUrl, setImgUrl] = useState("");
-  const [username, setUsername] = useState("");
-  const [bio, setBio] = useState("");
-  const dispatch = useDispatch();
+    const [imgUrl, setImgUrl] = useState("");
+    const [username, setUsername] = useState("");
+    const [bio, setBio] = useState("");
+    const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    dispatch(addProfile({ imgUrl, username, bio }));
+        try {
+            dispatch(addProfile({ imgUrl, username, bio }));
 
-    setImgUrl("");
-    setUsername("");
-    setBio("");
-  };
+            setImgUrl("");
+            setUsername("");
+            setBio("");
 
-  return (
-    <Container fluid={"sm"}>
-      <Card className="mb-3">
-        <Card.Header className="text-center">
-          <h5>Profile Info</h5>
-        </Card.Header>
-        <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-              <Form.Label>Image URL</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="The url for profile image"
-                value={imgUrl}
-                onChange={(e) => {
-                  setImgUrl(e.target.value);
-                }}
-                className="mb-3"
-                required
-              />
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="The url for profile image"
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                className="mb-3"
-                required
-              />
-              <Form.Group className="mb-3">
-                <Form.Label>Bio</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  placeholder="Talk about yourself"
-                  value={bio}
-                  onChange={(e) => {
-                    setBio(e.target.value);
-                  }}
-                  className="mb-3"
-                  maxLength={"256"}
-                  required
-                />
-              </Form.Group>
-              <Button type="submit" variant="primary">
-                Save Profile
-              </Button>
-            </Form.Group>
-          </Form>
-        </Card.Body>
-      </Card>
-      <hr />
-    </Container>
-  );
+            notification.addNotification({
+                title: "Success",
+                message: "Profile info updated!",
+                type: "success",
+                insert: "top",
+                container: "top-right",
+                animationIn: ["animate__animated", "animate__fadeIn"],
+                animationOut: ["animate__animated", "animate__fadeOut"],
+                dismiss: {
+                    duration: 5000,
+                    onScreen: true
+                }
+            });
+
+        } catch (error) {
+            notification.addNotification({
+                title: "Update profile failed",
+                message: `${error.message}`,
+                type: "danger",
+                insert: "top",
+                container: "top-right",
+                animationIn: ["animate__animated", "animate__fadeIn"],
+                animationOut: ["animate__animated", "animate__fadeOut"],
+                dismiss: {
+                    duration: 5000,
+                    onScreen: true
+                }
+            })
+        }
+
+    };
+
+    return (
+        <Container fluid={"sm"}>
+            <Card className="mb-3">
+                <Card.Header className="text-center">
+                    <h5>Profile Info</h5>
+                </Card.Header>
+                <Card.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Image URL</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="The url for profile image"
+                                value={imgUrl}
+                                onChange={(e) => {
+                                    setImgUrl(e.target.value);
+                                }}
+                                className="mb-3"
+                                required
+                            />
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="The url for profile image"
+                                value={username}
+                                onChange={(e) => {
+                                    setUsername(e.target.value);
+                                }}
+                                className="mb-3"
+                                required
+                            />
+                            <Form.Group className="mb-3">
+                                <Form.Label>Bio</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={3}
+                                    placeholder="Talk about yourself"
+                                    value={bio}
+                                    onChange={(e) => {
+                                        setBio(e.target.value);
+                                    }}
+                                    className="mb-3"
+                                    maxLength={"256"}
+                                    required
+                                />
+                            </Form.Group>
+                            <Button type="submit" variant="primary">
+                                Save Profile
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <hr />
+        </Container>
+    );
 };
 
 export default EditProfileInfoForm;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import 'react-notifications-component/dist/theme.css'
 import store from './redux/store.js';
 import { Provider } from 'react-redux'
+import { ReactNotifications } from 'react-notifications-component'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <ReactNotifications />
       <App />
     </Provider>
   </React.StrictMode>,
