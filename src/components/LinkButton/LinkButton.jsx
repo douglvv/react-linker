19d5ad2b098833590ca476b570202c58dd84ed79
Douglas Vidal <vidaldl01@gmail.com>
2023-07-06 17:09:25 -0300

    draggable buttons styles

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -3,50 +3,52 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { Button, ListGroup } from "react-bootstrap";
 import { updateLinks } from "../../redux/linksSlice/linksSlice";
 import { useDispatch } from "react-redux";
+import { MdDragHandle } from 'react-icons/md';
 
 const LinkButton = ({ links }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const onDragEnd = result => {
-        // console.log(result)
-        const { source, destination } = result;
-        
-        if(!destination) return;
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
 
-        const linksAux = Array.from(links);
-        const [movedLink] = linksAux.splice(source.index, 1);
-        linksAux.splice(destination.index, 0, movedLink);
-        
-        // console.log(linksAux)
+    if (!destination) return;
 
-        dispatch(updateLinks({links: linksAux}));
-    }
+    const linksAux = Array.from(links);
+    const [movedLink] = linksAux.splice(source.index, 1);
+    linksAux.splice(destination.index, 0, movedLink);
 
-    return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="buttons">
+    dispatch(updateLinks({ links: linksAux }));
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="buttons">
+        {(provided) => (
+          <ListGroup ref={provided.innerRef} {...provided.droppableProps}>
+            {links.map((link, index) => (
+              <Draggable key={link.btnUrl} index={index} draggableId={link.btnUrl}>
                 {(provided) => (
-                    <ul ref={provided.innerRef} {...provided.droppableProps}>
-                        {links.map((link, index) => (
-                            <Draggable key={link.btnUrl} index={index} draggableId={link.btnUrl}>
-                                {(provided) => (
-                                    <li
-                                        ref={provided.innerRef}
-                                        {...provided.dragHandleProps}
-                                        {...provided.draggableProps}
-                                        className="mb-3"
-                                    >
-                                        <a href={link.btnUrl}>{link.title}</a>
-                                    </li>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </ul>
+                  <Button
+                    className="mb-3 d-flex align-items-center"
+                    href={link.btnUrl}
+                    variant="primary"
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                  >
+                    <div {...provided.dragHandleProps}>
+                      <MdDragHandle className="fs-3"/>
+                    </div>
+                    <div className="flex-grow-1 text-center">{link.title}</div>
+                  </Button>
                 )}
-            </Droppable>
-        </DragDropContext>
-    );
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ListGroup>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 
 export default LinkButton;
