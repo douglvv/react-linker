0c033efda8c071606930f64ae28e3cd62adcde53
Douglas Vidal <87314898+douglvv@users.noreply.github.com>
2023-08-05 02:45:43 -0300

    Update Profile.jsx

--- src/components/Home/Profile/Profile.jsx
+++ src/components/Home/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './styles.module.css'
 import { Row, Col, Image, Badge } from "react-bootstrap";
-import { SlOptions, SlShare } from 'react-icons/Sl'
+import { SlOptions, SlShare } from 'react-icons'
 
 const Profile = ({ profile }) => {
     return (
